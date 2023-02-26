f24cefebcab5248352f4fa8f38c3b19b5e91c8f1
jun <junhuitee@hotmail.com>
2023-02-26 16:42:31 +0800

    add d5

--- 2022/d5/src/main.rs
+++ 2022/d5/src/main.rs
@@ -1,12 +1,12 @@
 use std::{
+    collections::VecDeque,
     fs::File,
     io::{BufRead, BufReader},
-    ops::Index,
 };
 
 fn main() {
     // read file
-    let filename = "./test";
+    let filename = "./input";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
     // get the crate on the top of each block
@@ -30,9 +30,11 @@ fn main() {
         }
     }
 
-    solution_1(first_half, second_half);
+    // solution_1(first_half.clone(), second_half.clone());
+    solution_2(first_half.clone(), second_half.clone());
 }
 
+#[derive(Debug)]
 struct Instruction {
     from: i32,
     to: i32,
@@ -55,23 +57,54 @@ impl CrateHandler {
         self.crates.push(some_crate);
     }
 
-    fn pop(&mut self) {
-        self.crates.pop();
+    fn pop(&mut self) -> Option<char> {
+        return self.crates.pop();
     }
-    
+
+    fn peek(&self) -> Option<&char> {
+        self.crates.last()
+    }
 }
 
-fn solution_1(first_half: Vec<String>, second_half: Vec<String>) -> i32 {
-    let crate_list = get_crate_list(first_half);
-    let direction_list = get_instruction_list(second_half);
+fn solution_1(first_half: Vec<String>, second_half: Vec<String>) {
+    let mut crate_list = get_crate_list(first_half);
+    let instruction_list = get_instruction_list(second_half);
 
-    1
+    for instruction in instruction_list {
+        crate_list = execute_instruction(instruction, crate_list);
+    }
+    for cc in crate_list {
+        match cc.peek() {
+            None => {
+                continue;
+            }
+            Some(c) => {
+                print!("{}", c);
+            }
+        }
+    }
+
 }
 
+fn solution_2(first_half: Vec<String>, second_half: Vec<String>) {
+    let mut crate_list = get_crate_list(first_half);
+    let instruction_list = get_instruction_list(second_half);
+
+    for instruction in instruction_list {
+        crate_list = execute_instruction_2(instruction, crate_list);
+    }
+    for cc in crate_list {
+        match cc.peek() {
+            None => {
+                continue;
+            }
+            Some(c) => {
+                print!("{}", c);
+            }
+        }
+    }
+}
 fn get_crate_list(first_half: Vec<String>) -> Vec<CrateHandler> {
-    // for line in first_half {
-    //     println!("{} {}", line, line.len());
-    // }
     let n = first_half.len();
     let m = first_half[0].len();
     let mut crate_list = Vec::<CrateHandler>::new();
@@ -89,11 +122,10 @@ fn get_crate_list(first_half: Vec<String>) -> Vec<CrateHandler> {
         let mut temp = CrateHandler::new();
         match c {
             None => {}
-            Some(c) => {
-                println!("found:{} {}", index, c);
+            Some(_) => {
                 for i in (0..n - 1).rev() {
                     let c = first_half[i].chars().nth(index).unwrap();
-                    if c.is_whitespace(){
+                    if c.is_whitespace() {
                         break;
                     }
                     temp.push(c);
@@ -109,12 +141,44 @@ fn get_instruction_list(second_half: Vec<String>) -> Vec<Instruction> {
     let mut instruction_list = Vec::<Instruction>::new();
     for line in second_half {
         let mut iter = line.split_whitespace();
-        // instruction_list.push(Instruction {
-        //     from,
-        //     to,
-        //     number,
-        // });
+        let number = iter.nth(1).unwrap().parse::<i32>().unwrap();
+        let from = iter.nth(1).unwrap().parse::<i32>().unwrap();
+        let to = iter.nth(1).unwrap().parse::<i32>().unwrap();
+        instruction_list.push(Instruction { from, to, number });
     }
-    todo!();
+
+    println!("{:?}", instruction_list);
     instruction_list
+}
+
+fn execute_instruction_2(
+    instruction: Instruction,
+    mut crate_list: Vec<CrateHandler>,
+) -> Vec<CrateHandler> {
+    let Instruction { from, to, number } = instruction;
+
+    let mut temp = VecDeque::<char>::new();
+    for _i in 0..number {
+        temp.push_front(crate_list[from as usize].pop().unwrap());
+    }
+
+    for _i in 0..number {
+        crate_list[to as usize].push(temp.pop_front().unwrap());
+    }
+
+    crate_list
+}
+fn execute_instruction(
+    instruction: Instruction,
+    mut crate_list: Vec<CrateHandler>,
+) -> Vec<CrateHandler> {
+    let Instruction { from, to, number } = instruction;
+
+    let mut temp = Vec::<char>::new();
+    for i in 0..number {
+        temp.push(crate_list[from as usize].pop().unwrap());
+        crate_list[to as usize].push(temp[i as usize]);
+    }
+
+    crate_list
 }
